91ea7fd3906606a7c5c73a3a66048cca7818cee8
satnam <satnamkahlon4@gmail.com>
2021-05-24 11:12:45 +0530

    Initial Commit

---
+++ A2_FA_iOS_Satnam_C0798066/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_FA_iOS_Satnam_C0798066
+//
+//  Created by SatnamSingh on 24/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
